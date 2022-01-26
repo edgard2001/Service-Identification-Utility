5b4766e8152cbfdaeaacbea3c6bd3cb01834c256
Edgard Romanovskiy <eromanovskiy303@gmail.com>
2022-01-26 17:41:59 +0000

    Added test cases for combinations(int, int) to UtilMathsTest

--- test/service/util/UtilMathsTest.java
+++ test/service/util/UtilMathsTest.java
@@ -7,17 +7,17 @@ import org.junit.jupiter.api.Test;
 class UtilMathsTest {
 
 	@Test
-	void testErroneous() {
+	void testFactorialErroneous() {
 		assertEquals(-1, UtilMaths.factorial(-1));
 	}
 	
 	@Test
-	void testBoundary() {
+	void testFactorialBoundary() {
 		assertEquals(1, UtilMaths.factorial(0));
 	}
 	
 	@Test
-	void testNormal() {
+	void testFactorialNormal() {
 		assertEquals(1, UtilMaths.factorial(1));
 		assertEquals(2, UtilMaths.factorial(2));
 		assertEquals(6, UtilMaths.factorial(3));
@@ -26,4 +26,19 @@ class UtilMathsTest {
 		assertEquals(720, UtilMaths.factorial(6));
 		assertEquals(5040, UtilMaths.factorial(7));
 	}
+	
+	@Test
+	void testCombinationsNormal() {
+		assertEquals(1, UtilMaths.combinations(2,2));
+		assertEquals(3, UtilMaths.combinations(3,2));
+		assertEquals(6, UtilMaths.combinations(4,2));
+		assertEquals(10, UtilMaths.combinations(5,2));
+		assertEquals(15, UtilMaths.combinations(6,2));
+		assertEquals(21, UtilMaths.combinations(7,2));
+		assertEquals(28, UtilMaths.combinations(8,2));
+		assertEquals(36, UtilMaths.combinations(9,2));
+		assertEquals(45, UtilMaths.combinations(10,2));
+	}
+	
+	
 }
